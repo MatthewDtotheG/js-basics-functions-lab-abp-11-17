0d68b37358179491e68d80adf7728d6fc7ccdc54
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-05 03:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function calculateVertical(start, end) {
+  let feetInBlock = 264;
+  let blocksTraveled = Math.abs(end - start);
+  retunr blocksTraveled * feetInBlock
+}
+
+function distanceFromHqInBlocks(end) {
+  let start = 42;
+  return Math.abs(end - start);
+}
+function distanceFromHqInFeet(end){
+  let blocksTraveled = distanceTravelledInFeet
+}
+
+let blockTraveled = Math.abs(end - start);
+
+return blocksTraveled * feetInBlock;
