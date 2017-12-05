0bdb8b2c615e3ace6a1c3a09206f43b8fc5cb864
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-05 04:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks (blockNumber) {
+function distanceFromHqInBlocks(blockNumber) {
   if (blockNumber > 42) {
     return blockNumber - 42;
   } else {
@@ -8,11 +8,11 @@ function distanceFromHqInBlocks (blockNumber) {
   }
 }
 
-function distanceFromHqInFeet (blockNumber) {
+function distanceFromHqInFeet(blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function distanceTravelledInFeet (start, destination) {
+function distanceTravelledInFeet(start, destination) {
   if (start < destination) {
     return (destination - start) * 264;
   } else {
@@ -20,13 +20,13 @@ function distanceTravelledInFeet (start, destination) {
   }
 }
 
-function calculatesFarePrice (start, destination) {
+function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
 
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return .02 * distance;
+    return 0.02 * distance;
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {
